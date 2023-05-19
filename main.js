0b2b53d19c89ce251ef05c915ed7bb6f7a217909
gitgit-AW <134073928+gitgit-AW@users.noreply.github.com>
2023-05-19 22:07:45 +0800

    Add files via upload
    Personal Website for Web Designing BSGD 3A

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const menuContainer = document.querySelector('.menu-container');
+const menuItems = document.querySelectorAll('.menu-container a');
+
+hamburgerIcon.addEventListener('click', () => {
+  menuContainer.classList.toggle('show');
+});
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', () => {
+    menuContainer.classList.remove('show');
+  });
+});
